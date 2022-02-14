2b5a52aca7795401a6e0988fd74a9ede3b9806a2
belousow_ilya <belousow.ilja@yandex.ru>
2022-02-14 20:17:58 +0500

    First step

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("my first game")
+import pygame
+
+pygame.init()
+
+W, H = 600, 400
+sc = pygame.display.set_mode((W, H))
+pygame.display.set_caption("My first game :)")
+
+pygame.draw.rect(sc, (255, 255, 255), (10, 10, 50, 100))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
