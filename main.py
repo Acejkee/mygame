58379bc5a4e91a1cf845cc00d51d727f4aaf34ff
belousow_ilya <belousow.ilja@yandex.ru>
2022-02-20 04:24:31 +0500

    Cool Game

--- main.py
+++ main.py
@@ -1,68 +1,46 @@
 import pygame
+from ball import Ball
+from random import randint
 
 pygame.init()
+pygame.time.set_timer(pygame.USEREVENT, 2000)
 
-W, H = 600, 400
+BLACK = (0, 0, 0)
 
-sc = pygame.display.set_mode((600, 400))
-pygame.display.set_caption("Изображения")
+W, H = 1200, 580
+sc = pygame.display.set_mode((W, H))
 
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-YELLOW = (239, 228, 176)
+bg = pygame.image.load('C:\\Users\\belou\Desktop\\images\\back1.jpg').convert()
 
 clock = pygame.time.Clock()
 FPS = 60
 
-car_surf = pygame.image.load(r"C:\Users\belou\Desktop\car.bmp").convert()
-bg_surf = pygame.image.load(r"C:\Users\belou\Desktop\sand.jpg").convert()
-finish_surf = pygame.image.load(r"C:\Users\belou\Desktop\finish.png").convert_alpha()
+balls_images = ['ball_bear.png', 'ball_fox.png', 'ball_panda.png']
+balls_surf = [pygame.image.load('C:\\Users\\belou\Desktop\\images\\' + path).convert_alpha() for path in balls_images]
 
-car_surf.set_colorkey((255, 255, 255))
-car_up = car_surf
-car_down = pygame.transform.flip(car_surf, 0, 1)
-car_left = pygame.transform.rotate(car_surf, 90)
-car_right = pygame.transform.rotate(car_surf, -90)
+balls = pygame.sprite.Group()
 
-car_rect = car_surf.get_rect(center=(W // 2, H // 2))
+def createBall(group):
+    indx = randint(0, len(balls_surf) - 1)
+    x = randint(20, W - 20)
+    speed = randint(1, 4)
+
+    return Ball(x, speed, balls_surf[indx], group)
 
 
-bg_surf = pygame.transform.scale(bg_surf, (bg_surf.get_width() // 3, bg_surf.get_height() // 3))
-
-car = car_up
-speed = 5
+createBall(balls)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+        elif event.type == pygame.USEREVENT:
+            createBall(balls)
 
-    bt = pygame.key.get_pressed()
-    if bt[pygame.K_LEFT]:
-        car = car_left
-        car_rect.x -= speed
-        if car_rect.x < 0:
-            car_rect.x = 0
-    elif bt[pygame.K_RIGHT]:
-        car = car_right
-        car_rect.x += speed
-        if car_rect.x > W - car_rect.height:
-            car_rect.x = W - car_rect.height
-    elif bt[pygame.K_UP]:
-        car = car_up
-        car_rect.y -= speed
-        if car_rect.y < 0:
-            car_rect.y = 0
-    elif bt[pygame.K_DOWN]:
-        car = car_down
-        car_rect.y += speed
-        if car_rect.y > H - car_rect.height:
-            car_rect.y = H - car_rect.height
-
-    sc.blit(bg_surf, (0, 0))
-    sc.blit(finish_surf, (0, 0))
-    sc.blit(car, car_rect)
-
+    sc.blit(bg, (0, 0))
+    balls.draw(sc)
     pygame.display.update()
 
     clock.tick(FPS)
+
+    balls.update(H)
