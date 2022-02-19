54aca4746fb0b089f54b0534fc60aa4fae7b13b7
belousow_ilya <belousow.ilja@yandex.ru>
2022-02-19 23:11:13 +0500

    Jump

--- main.py
+++ main.py
@@ -7,32 +7,44 @@ WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+
 sc = pygame.display.set_mode((W, H))
 pygame.display.set_caption("My first game :)")
 
 FPS = 60
 clock = pygame.time.Clock()
-x = W // 2
-y = H // 2
-speed = 5
-move = 0
+
+ground = H - 70
+jump_force = 20
+move = jump_force + 1
+
+hero = pygame.Surface((40, 50))
+hero.fill(BLUE)
+rect = hero.get_rect(centerx=W // 2)
+rect.bottom = ground
+
+rect_update = pygame.Rect(rect.x, 0, rect.width, ground)
+sc.fill(WHITE)
+pygame.display.update()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and event.mod == pygame.KMOD_LCTRL:
-                move = -speed
-            elif event.key == pygame.K_RIGHT and event.mod == pygame.KMOD_LCTRL:
-                move = speed
-        elif event.type == pygame.KEYUP:
-            if event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
-                move = 0
-    x += move
+            if event.key == pygame.K_SPACE and ground == rect.bottom:
+                move = -jump_force
 
-    sc.fill(WHITE)
-    pygame.draw.rect(sc, BLUE, (x, y, 10, 20))
-    pygame.display.update()
+        if move <= jump_force:
+            if rect.bottom + move < ground:
+                rect.bottom += move
+                if move < jump_force:
+                    move += 1
+            else:
+                rect.bottom = ground
+                move = jump_force + 1
+        sc.fill(WHITE)
+        sc.blit(hero, rect)
+        pygame.display.update(rect_update)
 
     clock.tick(FPS)
